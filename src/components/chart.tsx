397158f12260a30d94c09a8a9bbc493930d45a7d
/\|274 <77482155+arquirupim@users.noreply.github.com>
2023-03-14 11:53:27 +0900

    addedTooltip

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,11 +1,14 @@
 import { Group } from '@visx/group';
 import { weatherStore } from '../resource/js/store';
-import { extent, max, min } from 'd3-array';
-import { LinePath } from '@visx/shape';
+import { extent, max, min, bisect, bisector } from 'd3-array';
+import { Line, LinePath } from '@visx/shape';
 import { scaleTime, scaleLinear } from '@visx/scale';
 import {AxisLeft, AxisBottom} from '@visx/axis';
 import {GridRows, GridColumns} from '@visx/grid';
-import {useTooltip} from '@visx/tooltip';
+import {TooltipWithBounds, useTooltip, defaultStyles, Tooltip} from '@visx/tooltip';
+import {localPoint} from '@visx/event'
+import { useCallback } from 'react';
+import { GlyphCircle } from '@visx/glyph'
 
 interface CurveProps{
   width: number;
@@ -48,8 +51,6 @@ function Chart({ width = 1000, height = 800}: CurveProps) {
         domain: extent(weatherData, getTime) as [Date, Date],
         nice : true
     });
-    
-    console.log(extent(weatherData, getTime))
 
     const yScale = scaleLinear({
         range : [innerHeight, 0],
@@ -66,61 +67,160 @@ function Chart({ width = 1000, height = 800}: CurveProps) {
         hideTooltip 
     } = useTooltip();
 
+    const getD = (time : WeatherData) => {
+        const output = weatherData.filter((el)=>{
+            return el.time === time
+        })
+        return output
+    }
+
+    const bisectTemp = bisector(function(d) {return d.time}).left
+
+    const handleTooltip = useCallback((event) => {
+        const {x} = localPoint(event) || { x:0 };
+        const x0 = xScale.invert( x - margin.left )
+
+        const index = bisectTemp(weatherData, x0, 1);
+        console.log(index)
+
+        const d0 = weatherData[index - 1];
+        const d1 = weatherData[index];
+        let d = d0;
+
+        if(d1 && getTime(d1)){
+            d = x0.valueOf() - getTime(d0).valueOf() >
+            getTime(d1).valueOf() - x0.valueOf() ? d1 : d0;
+        }
+        showTooltip({
+            tooltipData : getD(d.time),
+            tooltipLeft : x + margin.left,
+            tooltipTop : yScale(getTemp(d))
+        })
+    })
+
+    const tooltipStyles = {
+        ...defaultStyles,
+        backgroundColor: 'rgba(53,71,125,0.8)',
+        color: 'white',
+        width: 200,
+        height: 100,
+      };
+
     return(
-    <svg width={width} height={height} >
-        <rect x={0} y={0} width={width} height={height} fill={'#718096'} rx={14} />
-        
-        <Group left={margin.left} top={margin.top}>
+        <div className='chartWrap'>
+        <svg width={width} height={height} >
             
-        <rect x={0} y={0} width={innerWidth} height={innerHeight} fill={'#A0AEC0'} />
-        <AxisLeft
-            tickTextFill={'#EDF2F7'}
-            stroke={'#EDF2F7'}
-            tickStroke={'#EDF2F7'}
-            scale={yScale}
-            tickLabelProps={() => ({
-                fill: '#EDF2F7',
-                fontSize: 11,
-                textAnchor: 'end',
-            })} 
-        />
-        <AxisBottom
-            scale={xScale}
-            stroke={'#EDF2F7'}
-            tickFormat={formatDate}
-            tickStroke={'#EDF2F7'}
-            tickTextFill={'#EDF2F7'}
-            top={innerHeight}
-            tickLabelProps={() => ({
-                fill: '#EDF2F7',
-                fontSize: 11,
-                textAnchor: 'middle',
-            })} 
-        />
-        <GridRows 
-            scale={yScale} 
-            width={innerWidth} 
-            height={innerHeight - margin.top} 
-            stroke='#EDF2F7' 
-            strokeOpacity={0.2} 
-        />
-        <GridColumns 
-            scale={xScale} 
-            width={innerWidth} 
-            height={innerHeight} 
-            stroke='#EDF2F7' 
-            strokeOpacity={0.2} 
-        />
-        <LinePath
-            key={1}
-            data={weatherData}
-            strokeWidth={3}
-            stroke={colors[1]}
-            x={(d) => xScale(getTime(d)) ?? 0}
-            y={(d) => yScale(getTemp(d)) ?? 0}
-        />
-        </Group>
-    </svg>
+            <rect x={0} y={0} width={width} height={height} fill={'#718096'} rx={14} />
+            
+            <Group left={margin.left} top={margin.top}>
+                
+            <rect x={0} y={0} width={innerWidth} height={innerHeight} fill={'#A0AEC0'} />
+            <rect
+                x={0}
+                y={0}
+                width={innerWidth}
+                height={innerHeight} 
+                fill={'transparent'}
+                onTouchStart={handleTooltip} 
+                onTouchMove={handleTooltip}
+                onMouseMove={handleTooltip}
+                onMouseLeave={() => hideTooltip()}
+            />
+            <AxisLeft
+                tickTextFill={'#EDF2F7'}
+                stroke={'#EDF2F7'}
+                tickStroke={'#EDF2F7'}
+                scale={yScale}
+                tickLabelProps={() => ({
+                    fill: '#EDF2F7',
+                    fontSize: 11,
+                    textAnchor: 'end',
+                })} 
+            />
+            <AxisBottom
+                scale={xScale}
+                stroke={'#EDF2F7'}
+                tickFormat={formatDate}
+                tickStroke={'#EDF2F7'}
+                tickTextFill={'#EDF2F7'}
+                top={innerHeight}
+                tickLabelProps={() => ({
+                    fill: '#EDF2F7',
+                    fontSize: 11,
+                    textAnchor: 'middle',
+                })} 
+            />
+            <GridRows 
+                scale={yScale} 
+                width={innerWidth} 
+                height={innerHeight - margin.top} 
+                stroke='#EDF2F7' 
+                strokeOpacity={0.2} 
+            />
+            <GridColumns 
+                scale={xScale} 
+                width={innerWidth} 
+                height={innerHeight} 
+                stroke='#EDF2F7' 
+                strokeOpacity={0.2} 
+            />
+            <LinePath
+                key={1}
+                data={weatherData}
+                strokeWidth={3}
+                stroke={colors[1]}
+                x={(d) => xScale(getTime(d)) ?? 0}
+                y={(d) => yScale(getTemp(d)) ?? 0}
+                fill="transparent"
+            />
+            </Group>
+            {/* tooltip */}
+            {tooltipData && (
+            <g>
+                <Line
+                    from={{x: tooltipLeft - margin.left, y:margin.top}}
+                    to={{ x: tooltipLeft - margin.left, y:height - margin.bottom}}
+                    stroke={'#EDF2F7'}
+                    strokeWidth={2}
+                    pointerEvents="none"
+                    strokeDasharray="4,2"
+                />
+            </g>
+            )}
+            {tooltipData && tooltipData.map((d, i) => (
+                <g>
+                    <GlyphCircle
+                        left={tooltipLeft - margin.left}
+                        top={yScale(d.temperature) + margin.top}
+                        size={110}
+                        fill={"#EDF2F7"}
+                        stroke={'white'}
+                        strokeWidth={2}
+                    />
+                </g>
+            ))}
+        </svg>
+        <div className='tooltip'>
+        {tooltipData && (
+            <div>
+            <TooltipWithBounds
+                key={Math.random()}
+                top={tooltipTop}
+                left={tooltipLeft}
+                style={tooltipStyles}
+            >
+             <p>{`Time: ${formatDate(getTime(tooltipData[0]))}`}</p>
+             <p>{`Temp: ${getTemp(tooltipData[0])}ºC`}</p>
+             <p>{`Weather: ${getWeather(tooltipData[0])}`}</p>
+            </TooltipWithBounds>
+            
+            </div>
+            
+        )}
+
+        </div>
+        </div>
+    
     );
 }
 
